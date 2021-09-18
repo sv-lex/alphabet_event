4f2138a66218acbe8274c7916d16055e1a3b6a26
DiZZert <dizzertmaster@gmail.com>
2021-09-18 05:34:40 +0300

    вложенные в .blockalphabet div'ы имеют один стиль

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -15,27 +15,23 @@
   var abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
   for (let i = 0; i < abc.length; i++) {
-
       $( '<div>', {class: 'char' + i + ''})
       .html( `<img src="./image/letters/` + abc[i] + `.png" width="110" height="150">` ).appendTo( $charString )
       .click(function() {
         console.log(i);
         alert(abc[i]);
       });
-
   }
 
+//добавляем аватары на нужные позиции
   for (let i = 0; i < players.length; i++) {
-
     let $currentDiv = $( '.char' + players[i].position);
-
     $( '<div>', {class: 'avatarDiv'} )
     .html( `<img src="./image/avatars/` + players[i].name + `.png" width="40%">` ).appendTo( $currentDiv )
     .click(function() {
       console.log(i);
       alert(players[i].name);
     });
-
   }
 
 } )( jQuery );
