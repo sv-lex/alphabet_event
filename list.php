fea8009c961e5eefdcb1d9b73c5d4ae65d8c59e6
dizzert <dizzertmaster@gmail.com>
2021-09-22 09:59:56 +0300

    list css

--- list.php
+++ list.php
@@ -8,42 +8,41 @@
     </head>
     <body>
 
-      <?php
-      $letter = !empty($_GET['letter']) ? $_GET['letter'] : 'логин не передан!';
+      <div class="gameslist">
 
-      // $letter = !empty($_GET['letter']) ? $_GET['letter'];
+        <?php
+        $letter = !empty($_GET['letter']) ? $_GET['letter'] : 'логин не передан!';
 
-      $user = "dizzert";
-      $password = "megaPasswd1";
-      $database = "alphabet";
-      $table = "games";
+        $user = "dizzert";
+        $password = "megaPasswd1";
+        $database = "alphabet";
+        $table = "games";
 
-      try {
-        $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-        echo "<table>";
-        foreach($db->query("SELECT * FROM `games` WHERE `game_letter` = '$letter'") as $row) {
-            // echo "<td>" . $row['game_name'] . "</td>";
-            echo "
-            <tr>
-              <td>" . $row['game_name'] . "</td>
-              <td> / </td>
-              <td> <a href='https://store.steampowered.com/app/" . $row['steam_appid'] . "'>steam</a> </td>
-              <td> / </td>
-              <td> <a href='https://howlongtobeat.com/game?id=" . $row['hltb_id'] . "'>hltb</a> </td>
-              <td> / </td>
-              <td> <a href='https://www.metacritic.com/game/pc/" . $row['metacritic_url'] . "'>metacritic</a> </td>
-            </tr>";
+        try {
+          $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+          echo "<table>";
+          foreach($db->query("SELECT * FROM `games` WHERE `game_letter` = '$letter'") as $row) {
+              echo "
+              <tr>
+                <td>" . $row['game_name'] . "</td>
+                <td> / </td>
+                <td> <a href='https://store.steampowered.com/app/" . $row['steam_appid'] . "'>steam</a> </td>
+                <td> / </td>
+                <td> <a href='https://howlongtobeat.com/game?id=" . $row['hltb_id'] . "'>hltb</a> </td>
+                <td> / </td>
+                <td> <a href='https://www.metacritic.com/game/pc/" . $row['metacritic_url'] . "'>metacritic</a> </td>
+              </tr>";
+          }
+          echo "</table>";
+
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
         }
-        echo "</table>";
 
-      } catch (PDOException $e) {
-          print "Error!: " . $e->getMessage() . "<br/>";
-          die();
-      }
+        ?>
 
-      ?>
-
-      <!-- <p> the letter is:  <?= $letter ?> </p> -->
+       </div>
 
     </body>
 </html>
